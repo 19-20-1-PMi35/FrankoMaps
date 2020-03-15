cbebf050d0a1df050bd1ebb3c2914a7328a74ff6
YuriyDyakiv <yuradyakiv@gmail.com>
2020-03-15 15:21:50 +0200

    add database

---
+++ DataAccess/Entities/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Entities
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public List<Map> Maps { get; set; }
+        public List<Point> Points { get; set; }
+        public List<Distance> Distances { get; set; }
+    }
+}
---
+++ DataAccess/Entities/Distance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Entities
+{
+    public class Distance
+    {
+        public int Id { get; set; }
+        public int From { get; set; }
+        public int To { get; set; }
+        public double Weight { get; set; }
+
+        public int AdminId { get; set; }
+        public List<Point> Points { get; set; }
+    }
+}
