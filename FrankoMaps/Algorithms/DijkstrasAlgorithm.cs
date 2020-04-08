84955f01849d0e6fb312261f5a3046741d6eee8c
Yurii Diakiv <yuradyakiv@gmail.com>
2020-04-08 21:58:37 +0300

    add new feature to return the full path

--- FrankoMaps/Algorithms/DijkstrasAlgorithm.cs
+++ FrankoMaps/Algorithms/DijkstrasAlgorithm.cs
@@ -4,8 +4,10 @@ namespace FrankoMaps.Algorithms
 {
     public class DijkstrasAlgorithm
     {
+        private readonly int NO_PARENT = -1;
         private readonly int nVertices;
         private readonly double[,] graph;
+        private int[] parents;
         private double[] shortestDistances;
         public DijkstrasAlgorithm(double[,] _graph)
         {
@@ -25,6 +27,10 @@ namespace FrankoMaps.Algorithms
             }
 
             shortestDistances[indexOfStartVertex] = 0;
+
+            parents = new int[nVertices];
+
+            parents[indexOfStartVertex] = NO_PARENT;
 
             // Find shortest path for all vertices 
             for (int count = 0; count < nVertices - 1; count++)
@@ -48,9 +54,27 @@ namespace FrankoMaps.Algorithms
                     if (!shortestPathTreeSet[v] && graph[nearestVertex, v] != 0 && minDistance != int.MaxValue
                         && shortestDistances[nearestVertex] + graph[nearestVertex, v] < shortestDistances[v])
                     {
+                        parents[v] = nearestVertex;
                         shortestDistances[v] = shortestDistances[nearestVertex] + graph[nearestVertex, v];
                     }
                 }
+            }
+        }
+
+        public List<int> GetPathForVertex(int vertex)
+        {
+            List<int> path = new List<int>();
+            MakePath(vertex);
+            return path;
+
+            void MakePath(int currentVertex)
+            {
+                if (currentVertex == NO_PARENT)
+                {
+                    return;
+                }
+                MakePath(parents[currentVertex]);
+                path.Add(currentVertex);
             }
         }
     }
