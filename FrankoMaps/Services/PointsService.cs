5492bdb88199b84176898f29417fc0a485c3900e
Yurii Diakiv <yuradyakiv@gmail.com>
2020-04-12 10:43:45 +0300

    add maps controller, view (draw points from db)

--- FrankoMaps/Services/PointsService.cs
+++ FrankoMaps/Services/PointsService.cs
@@ -1,5 +1,8 @@
 using DataAccess.Repositories;
 using AutoMapper;
+using System.Collections.Generic;
+using FrankoMaps.Models;
+using DataAccess.Entities;
 
 namespace FrankoMaps.Services
 {
@@ -12,6 +15,13 @@ namespace FrankoMaps.Services
         {
             repository = pointRepository;
             _mapper = mapper;
+        }
+
+        public List<PointViewModel> GetPoints()
+        {
+            List<Point> points = repository.GetItems();
+            List<PointViewModel> pointViewModels = _mapper.Map<List<PointViewModel>>(points);
+            return pointViewModels;
         }
     }
 }
